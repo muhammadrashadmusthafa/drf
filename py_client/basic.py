f816511eb84e1a2d0cafff8fda9c563bbec0887d
ichu <muhammadrashadmusthafa@gmail.com>
2022-09-04 02:34:27 +0530

    first api view

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,13 +1,18 @@
 import requests
 
-endpoint = "http://localhost:8000/"
+endpoint = "http://localhost:8000/api/"
 
 # endpoint = "https://httpbin.org/anything"
-get_response = requests.get(endpoint)
+# get_response = requests.get(endpoint)
 
-# get_response = requests.get(endpoint, json={'query': 'hello world'})
+get_response = requests.get(endpoint, json={'query': 'hello world'})
 # get_response = requests.get(endpoint, data={'query': 'hello world'})
 
 print(get_response.text)
-# print(get_response.json())
+print(type(get_response.text))
+
+print(get_response.json())
+print(type(get_response.json()))
+
+print(get_response.json()['message'])
 print(get_response.status_code)
