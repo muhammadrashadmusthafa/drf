e0e376a838bc2338b2d26c36eea98d6f0bfc9988
ichu <muhammadrashadmusthafa@gmail.com>
2022-09-06 18:11:00 +0530

    django model instance as api response

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -1,24 +1,15 @@
 from django.http import JsonResponse
 import json
 
+from products.models import Product
+
 
 def api_home(request, *args, **kwargs):
-    # request -> HttpRequest -> Django
-    # print(dir(request))
-    # request.body
-    print(request.GET) # url query params
-    print(request.POST)
-    print(request.headers)
-    body = request.body # byte string of JSON data
-    print(body)
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body) # string of JSON data -> Python Dict
-    except:
-        pass
-    print(data)
-    # data['headers'] = request.headers # request.META ->
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
     return JsonResponse(data)
