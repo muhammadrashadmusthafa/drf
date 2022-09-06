6b6bd1466ea7e3d35348bd61596c83ec462dc78f
ichu <muhammadrashadmusthafa@gmail.com>
2022-09-06 14:56:37 +0530

    echo data

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -1,5 +1,24 @@
 from django.http import JsonResponse
+import json
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this is your Django API response!!"})
+    # request -> HttpRequest -> Django
+    # print(dir(request))
+    # request.body
+    print(request.GET) # url query params
+    print(request.POST)
+    print(request.headers)
+    body = request.body # byte string of JSON data
+    print(body)
+    data = {}
+    try:
+        data = json.loads(body) # string of JSON data -> Python Dict
+    except:
+        pass
+    print(data)
+    # data['headers'] = request.headers # request.META ->
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,14 +5,8 @@ endpoint = "http://localhost:8000/api/"
 # endpoint = "https://httpbin.org/anything"
 # get_response = requests.get(endpoint)
 
-get_response = requests.get(endpoint, json={'query': 'hello world'})
-# get_response = requests.get(endpoint, data={'query': 'hello world'})
-
-print(get_response.text)
-print(type(get_response.text))
+get_response = requests.get(endpoint, json={"product_id": 123 }, params= {'abc': 123}) # HTTP Request
+# print(get_response.text) # print raw text response
+# print(get_response.status_code)
 
 print(get_response.json())
-print(type(get_response.json()))
-
-print(get_response.json()['message'])
-print(get_response.status_code)
