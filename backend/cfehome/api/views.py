aa3c14479752d178d30fb1a567608600849b7acf
ichu <muhammadrashadmusthafa@gmail.com>
2022-09-06 18:18:02 +0530

    django model instance to dictionary

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 import json
+from django.forms.models import model_to_dict
 
 from products.models import Product
 
@@ -8,8 +9,9 @@ def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
     return JsonResponse(data)
+    #     print(data)
+    #     data = dict(data)
+    #     json_data_str = json.dumps(data)
+    # return HttpResponse(json_data_str, headers={"content-type": "application/json"})
